5422c402e6f3b12442ce2fc6da5be0308a50a8f6
Nacho Torrella <nachotorrella@gmail.com>
2017-12-24 17:03:43 +0100

    review code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name: string;
+  email: string;
+  hobbies: string[];
+  showHobbies: boolean;
+
+  constructor () {
+    this.name = 'Nacho';
+    this.email = 'nachotorrella@gmail.com';
+    this.hobbies = ['eat', 'sleep', 'eat'];
+    this.showHobbies = false;
+  }
+
+  toggleHobbies = () => {
+    this.showHobbies = !this.showHobbies;
+  }
+
+  addHobby = (newHobby) => {
+    this.hobbies = [...this.hobbies, newHobby.value];
+    newHobby.value = '';
+    return false;
+  }
 }
